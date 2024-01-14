b24d1ebb8bfad0e8db5ad84bf2319530243cc24a
Archie <agajanov0arslan@gmail.com>
2024-01-14 10:36:48 +0500

    notask

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const tasks = [
-  {
-    _id: '5d2ca9e2e03d40b326596aa7',
-    completed: true,
-    body:
-      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-    title: 'Eu ea incididunt sunt consectetur fugiat non.',
-  },
-  {
-    _id: '5d2ca9e29c8a94095c1288e0',
-    completed: false,
-    body:
-      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title:
-      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-  },
-  {
-    _id: '5d2ca9e2e03d40b3232496aa7',
-    completed: true,
-    body:
-      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-    title: 'Eu ea incididunt sunt consectetur fugiat non.',
-  },
-  {
-    _id: '5d2ca9e29c8a94095564788e0',
-    completed: false,
-    body:
-      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title:
-      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-  },
+  // {
+  //   _id: '5d2ca9e2e03d40b326596aa7',
+  //   completed: true,
+  //   body:
+  //     'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+  //   title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  // },
+  // {
+  //   _id: '5d2ca9e29c8a94095c1288e0',
+  //   completed: false,
+  //   body:
+  //     'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+  //   title:
+  //     'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  // },
+  // {
+  //   _id: '5d2ca9e2e03d40b3232496aa7',
+  //   completed: true,
+  //   body:
+  //     'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+  //   title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  // },
+  // {
+  //   _id: '5d2ca9e29c8a94095564788e0',
+  //   completed: false,
+  //   body:
+  //     'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+  //   title:
+  //     'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  // },
 ];
 
 (function(arrOfTasks){
@@ -36,6 +36,7 @@ const tasks = [
     acc[task._id] = task;
     return acc;
   },{});
+  console.log(objOfTasks);
   //Element UI - user interface;
   const listContainer = document.querySelector('.tasks-list-section .list-group')
   const form = document.forms['addTask'];
@@ -97,7 +98,11 @@ function listItemTemplate ({_id,title,body}){
     //Нужно сделать динамичный сборщик классов
 }
 function renderAllTasks(objOfTasks){
-  if(!objOfTasks){
+  if(Object.keys(objOfTasks).length === 0){
+    const netZadach = document.createElement('h1');
+    netZadach.textContent = 'ВСЕ СДЕЛАНО!!! ПРОДОЛЖАЙ ТАК ЖЕ!!!';
+    netZadach.classList.add('display-1', 'mx-auto', 'text-center', 'text-success');
+    listContainer.appendChild(netZadach);
     console.log('Нет задач');
     return;
   }//Проверка на наличие обьектов
