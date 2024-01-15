1c94c713f9c62b39ab4db9e861153730508b5a64
Archie <agajanov0arslan@gmail.com>
2024-01-15 20:38:47 +0500

    themeValue

--- app.js
+++ app.js
@@ -40,18 +40,91 @@ const tasks = [
     // }
     return acc;
   }, {});
+
+  const themes = {
+    default: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#007bff',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#007bff',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#0069d9',
+      '--default-btn-border-color': '#0069d9',
+      '--danger-btn-bg': '#dc3545',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#bd2130',
+      '--danger-btn-border-color': '#dc3545',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#80bdff',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
+    },
+    dark: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#343a40',
+      '--dark-theme': '#161616',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#58616b',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#292d31',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#b52d3a',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#88222c',
+      '--danger-btn-border-color': '#88222c',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+    light: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#fff',
+      '--header-text-color': '#212529',
+      '--default-btn-bg': '#fff',
+      '--default-btn-text-color': '#212529',
+      '--default-btn-hover-bg': '#e8e7e7',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#f1b5bb',
+      '--danger-btn-text-color': '#212529',
+      '--danger-btn-hover-bg': '#ef808a',
+      '--danger-btn-border-color': '#e2818a',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+  };
+  let lastSelectedTheme = 'default';
+
   //Elements UI - user interface;
   const listContainer = document.querySelector('.tasks-list-section .list-group');
   const form = document.forms['addTask'];
   const inputTitle = form.elements['title'];
   const inputBody = form.elements['body'];
+  const selectedTheme = document.getElementById('themeSelect');
   const filterContainer = document.querySelector('.filterContainer');
 
   //events
   renderAllTasks(objOfTasks);
   form.addEventListener('submit', onSubmitHandler);
   listContainer.addEventListener('click', onDeleteHandler);
-  filterContainer.addEventListener('click', filterTodos);
+  selectedTheme.addEventListener('change', onThemeChangeHandler);
+  // filterContainer.addEventListener('click', filterTodos);
+
 
   function renderAllTasks(objOfTasks) {
     if (!objOfTasks) {
@@ -125,7 +198,28 @@ const tasks = [
       if (confirmed) parent.remove();
     }
   }
-  function filterTodos({ target }) {
-    
+  // function filterTodos({ target }) {
+  //   if(target.classList.contains('btn-primary')) {
+  //     const filteredTasks = tasks.filter(task=>{
+  //       return task.completed;
+  //     });
+  //     return filteredTasks;
+  //   }
+  // }
+  function onThemeChangeHandler(e) {
+    const selectedValue = selectedTheme.value;
+    const isConfirm = confirm(`Вы дейст хотите выбрать тему ${selectedValue}`);
+    if(!isConfirm) {
+      selectedValue = lastSelectedTheme;
+      return;
+    };
+    setTheme(selectedValue);
   }
+  function setTheme(name) {
+    const selectedThemeObj = themes[name];
+    Object.entries(selectedThemeObj).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    });
+  }
+
 })(tasks);
